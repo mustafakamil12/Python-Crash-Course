89505e2d98cf10676cdbeb541c883a2d4bbfd41c
Mustafa Al Ogaidi <mustafaalogaidi@Mustafas-MacBook-Air.local>
2021-09-13 22:32:47 -0500

    done with strings in chapter-2

---
+++ apostrophe.py
@@ -0,0 +1,5 @@
+message = "One of Python's strengths is itsdiverse community."
+print(message)
+
+famous_person = " Albert Einstein "
+print(f'{famous_person.strip()} once said, "A person who never made a mistake never tried any-\nthing new."')
--- stripping_whitespaces.py
+++ stripping_whitespaces.py
@@ -3,3 +3,7 @@ print(favorite_language)
 
 print(favorite_language.rstrip()) # remove right space
 print(favorite_language.lstrip()) # remove left space
+
+print(f"The original string still the same {favorite_language}")
+
+print(favorite_language.strip()) # remove left and right white space.
